9c7f6594142303d0714722f7e3bf47988746f65b
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-02-23 20:32:09 -0300

    adding authenticate use case and their automated tests

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from './controllers/authenticateController'
 import { createUser } from './controllers/createUserController'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', createUser)
+  app.post('/sessions', authenticate)
 }
