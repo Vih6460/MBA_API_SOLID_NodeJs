0fc6dd1dedcd9b0a8cfca2d15816146eb188790d
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-02-23 19:35:17 -0300

    making first switch of automated unity tests

---
+++ src/use-cases/createUser.spec.ts
@@ -0,0 +1,59 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { compare } from 'bcryptjs'
+import { expect, describe, it } from 'vitest'
+import { CreateUserUseCase } from './createUser'
+
+describe('Register Use Case', () => {
+  it('should be able to create user', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const createUserUseCase = new CreateUserUseCase(usersRepository)
+
+    const { user } = await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const createUserUseCase = new CreateUserUseCase(usersRepository)
+
+    const { user } = await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to create user with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const createUserUseCase = new CreateUserUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await createUserUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      createUserUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
