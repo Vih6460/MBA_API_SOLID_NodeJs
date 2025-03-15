c46d7992ec35d1625006ce2287c1c6228a25491a
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-03-15 19:49:44 -0300

    implementing JWT (JSON-WEB_TOKEN)

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticateController'
 import { createUser } from './controllers/createUserController'
+import { profile } from '@/http/controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', createUser)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
