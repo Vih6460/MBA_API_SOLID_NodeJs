b113748a5a19702204057b04e83e6f4d40e5bf33
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-03-15 19:59:24 -0300

    adding middleware to automaticate the JWT validation

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticateController'
 import { createUser } from './controllers/createUserController'
 import { profile } from '@/http/controllers/profile'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', createUser)
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
