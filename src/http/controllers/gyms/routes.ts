8ca4dade95245a67e563721a4a1f3f7033a85412
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-04-05 18:45:31 -0300

    adding nearby and search controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { search } from './searchController'
+import { nearby } from './nearbyController'
+import { createGym } from './createGymController'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', createGym)
 }
